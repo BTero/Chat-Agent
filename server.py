cbb79e303aa5dec05713bceeb4c9469bc99c2c9d
Brian Tero <Btero@uci.edu>
2015-05-28 22:05:12 -0700

    Prevents another client from joining a current session. Adds client when agent is available.

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import sys
 
 done = False
 clients = []
+wait_list = []
 
 def broadcast(data):
     global clients
@@ -11,16 +12,31 @@ def broadcast(data):
     for c in clients:
         c.do_send(data)
 
+def get_first(iterable, default=None):
+    if iterable:
+        for item in iterable:
+            return item
+        return default
+
 class MyHandler(Handler):
     
     def on_open(self):
-        global clients
+        global clients, wait_list
         if len(clients) < 2:
             clients.append(self)
+        else:
+            self.do_send("Agent is currently busy. Please wait until the Agent is available.")
+            wait_list.append(self)
         
     def on_close(self):
         global clients
+        global wait_list
         clients.remove(self)
+        if wait_list:
+            w = get_first(wait_list)
+            w.do_send("You are now speaking with the Agent.")
+            clients.append(w)
+            wait_list.remove(w)
 
     def on_msg(self, data):
         global clients
@@ -28,8 +44,9 @@ class MyHandler(Handler):
             global done
             done = False
         else:
-            broadcast(data)
-            print(data)
+            if self in clients:
+                broadcast(data)
+                print(data)
 
 port = 8888
 server = Listener(port, MyHandler)
