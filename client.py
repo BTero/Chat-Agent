d434db3cff25402baae11be275c7918b464ef380
Brian Tero <Btero@uci.edu>
2015-05-31 23:49:31 -0700

    Finalized version turned into class.

--- client.py
+++ client.py
@@ -1,10 +1,12 @@
 from network import Handler, poll
 from threading import Thread
 from time import sleep
+from PIL import Image
 import sys
 
 done = False
 # displayed = False
+im = Image.open("sam.png")
 
 def periodic_poll():
     while 1:
@@ -15,24 +17,30 @@ def periodic_poll():
 def options():
     # global displayed
     # displayed = True
-    print("[1] Complaint\n[2] Question\n[3] Other\n")
+    print("[1] Complaint\n[2] Question\n[3] Other\n[4] Quit \n")
     choice = int(raw_input("Choose one of the above:"))
     if choice == 1:
         print("Connecting to agent...")
         client.do_send("Client Option: complaint.")
+        print("Connected")
 
     elif choice == 2:
         client.do_send("Client Option: question")
         userTopicInput = raw_input("Enter your topic: ")
         client.do_send((str(userTopicInput).split(' ')))
         print("Connecting to agent...")
+        print("Connected")
 
     elif choice == 3:
         print("Connecting to agent...")
         client.do_send("Client Option: other")
+        print("Connected")
 
+    elif choice == 4:
+        print("Quitting")
+        client.do_close()
     else:
-        print("You need to pick options 1, 2, or 3!")
+        print("You need to pick options 1, 2, 3 or 4")
         options()
 
 transcript = []
@@ -44,6 +52,13 @@ def save_transcript():
         for s in transcript:
             text_file.write(str(s))
 
+def send_message(choiceInput):
+    append_data_to_transcript(choiceInput)
+    client.do_send(choiceInput)
+
+def show_image():
+    im.show()
+
 class Client(Handler):
     in_chat = False
 
@@ -52,19 +67,26 @@ class Client(Handler):
         # print ('Client started')
 
     def on_close(self):
+        global done
+        done = True
         print ('Client closed')
+        self.do_send('QUIT')
 
     def on_msg(self, data):
+        if '!!QUIT!!' in data:
+            global done
+            done = True
+            client.on_close()
         # if 'Choose one of the above:' in data:
         #     choice = int(raw_input())
         #     self.do_send('Client Option: ' + choice)
         append_data_to_transcript(data)
         print data
 
-ip = '68.5.75.43' # Yuki's host
+# ip = '68.5.75.43' # Yuki's host
 portn = 8888
 # ip = '169.234.104.169'
-# ip = 'localhost'
+ip = 'localhost'
 # ip = str(raw_input("Please enter the IP Address you would like to connect to: "))
 # host, port = '192.168.0.19', 8888 #laptop host
 # host, port = 'cs137.dtdns.net', 8888
@@ -89,9 +111,11 @@ while not done:
         done = True
     elif ':s' in choiceInput:
         save_transcript()
+    elif ':e' in choiceInput:
+        show_image()
     else:
         choiceInput = 'Client: ' + choiceInput
-        client.do_send(choiceInput)
+        send_message(choiceInput)
 
 client.do_close()# cleanup
 sys.exit()
--- server.py
+++ server.py
@@ -1,21 +1,33 @@
 from network import Listener, Handler, poll, get_my_ip
+from threading import Thread
+from time import sleep
 import sys
 
 done = False
 clients = []
 wait_list = []
 
-def broadcast(data):
+def periodic_poll():
+    while 1:
+        poll()
+        sleep(0.05)  # seconds
+
+def broadcast(self, data):
     global clients
 
     for c in clients:
-        c.do_send(data)
+        if self != c:
+            c.do_send(data)
 
 def get_first(iterable, default=None):
     if iterable:
         for item in iterable:
             return item
         return default
+
+def get_wait_list():
+    global wait_list
+    return wait_list
 
 # def check_wait_list(client):
 #     global wait_list
@@ -33,13 +45,14 @@ class MyHandler(Handler):
             clients.append(self)
             # display_options(self)
         else:
-            self.do_send("Agent is currently busy. Please wait until the Agent is available.")
+            self.do_send("\nAgent is currently busy. Please wait until the Agent is available.")
             wait_list.append(self)
-        
+
     def on_close(self):
         global clients
         global wait_list
         clients.remove(self)
+        print('after on_close' + str(len(clients)))
         if wait_list:
             w = get_first(wait_list)
             w.do_send("You are now speaking with the Agent.")
@@ -53,14 +66,29 @@ class MyHandler(Handler):
             done = True
         else:
             if self in clients:
-                broadcast(data)
+                broadcast(self, data)
                 print(data)
 
-port = 8888
-server = Listener(port, MyHandler)
-print ('Server started')
-print 'Server IP Address: ' + str(get_my_ip())
-while not done:
-    poll(timeout=0.05)
-server.stop()  # cleanup
-sys.exit()
+
+def main():
+    done = False
+    port = 8888
+    server = Listener(port, MyHandler)
+    print ('Server started')
+    print 'Server IP Address: ' + str(get_my_ip())
+    print 'type QUIT to end the server'
+    while not done:
+        input = sys.stdin.readline().rstrip()
+        if 'QUIT' in input:
+            for c in clients:
+                c.do_send('!!QUIT!!')
+            done = True
+    server.stop()  # cleanup
+    sys.exit()
+
+
+thread = Thread(target=periodic_poll)
+thread.daemon = True  # die when the main thread dies
+thread.start()
+if __name__ == "__main__":
+    main()
